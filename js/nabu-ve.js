2c1100be5e393e6879b139314cb0a6e1b926bc34
nabu-3 PHP Framework <ragutimar@gmail.com>
2017-05-14 15:16:20 +0200

    Check Gravatar image

---
+++ js/nabu-ve.js
@@ -0,0 +1,6 @@
+/* Static load of mxGraph library */
+(function() {
+    mxBasePath = "/runtime/assets/mxgraph/3.7.2/src";
+    document.write('<script type="text/javascript" src="/runtime/assets/mxgraph/3.7.2/mxClient.min.js"></script>');
+    Nabu.LibraryManager.registerPackage('hola', 'feo');
+})();
